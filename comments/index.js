1a3155c4cbb4fd43fe25f049dbc312c7dd6a4dfa
Athanasios Kalampoukas <a.kalampoukas@netstudio.gr>
2022-11-06 19:18:35 +0200

    Creates comments service.

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+let id = 1;
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] ?? {});
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const { content } = req.body;
+    const comments = commentsByPostId[req.params.id] ?? {};
+    comments[id] = {
+        id,
+        content,
+    };
+    commentsByPostId[req.params.id] = comments;
+
+    id++;
+
+    res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
