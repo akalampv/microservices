9558713421d0f9f2527658cf914bb7030f6fbf56
Athanasios Kalampoukas <a.kalampoukas@netstudio.gr>
2022-11-06 18:36:49 +0200

    Creates posts service.

---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+let id = 1;
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    // const id = randomBytes(4).toString('hex');
+
+    const { title } = req.body;
+
+    posts[id] = {
+        id,
+        title,
+    };
+
+    id++;
+
+    res.status(201).send(posts[id-1]);
+});
+
+app.listen(4000, () => {
+    console.log('Listening on 4000');
+});
